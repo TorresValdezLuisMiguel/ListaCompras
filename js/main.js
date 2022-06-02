3d6f806d6458777f83590b925c9ebfd0e949d73b
LuisMiguel <torresvaldezluismiguel@gmail.com>
2022-06-02 15:54:52 -0500

    Se agrego el contador de productos por pieza/kg

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let contadorTabla=0;
 let costoTotal=0;
+let totalEnProductos=0;
 
 let element = document.getElementById("totalPrecio");
 element.innerHTML="Total en precio:";
@@ -97,6 +98,8 @@ agregar.addEventListener("click", (event)=>{
     document.getElementById("contadorProductos").innerHTML=contadorTabla;
     let precio = (Math.floor((Math.random()*50)*100))/100;
     let cantidad = parseFloat(txtNumber.value);
+    totalEnProductos+=Math.ceil(cantidad);
+    document.getElementById("productosTotal").innerHTML=totalEnProductos;
     costoTotal+=(precio*cantidad);
     total.innerHTML=`$ ${costoTotal.toFixed(2)}`;
         let temp =`<tr>
